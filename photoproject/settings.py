09f36a575d3916b790713ffedd6d932ab8b308c9
fakename <fake@example.com>
2023-11-06 14:24:14 +0900

    Change settings

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-atat@2k8x@$+wy_@d*%e!ja+ob%bf18i3$j*r7x_%0!ttzu$&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)    # BASE_DIR → C://~/photoproject
+STATIC_ROOT = '/home/ubuntu/static'        # 本番環境でstaticフォルダーを配置する場所
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
